f3333085901a3711a44c79e853fde9549926c68c
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-07-31 14:59:25 +0000

    Implemented logout functionality and dynamic view rendering for auth

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
