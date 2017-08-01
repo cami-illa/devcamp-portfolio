3982be29187eb34394652563e17c855e20f19df6
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-08-01 15:59:24 +0000

    refactored strong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,32 +13,33 @@ class PortfoliosController < ApplicationController
     end
     
     def create
-     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
-
-    respond_to do |format|
-      if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item  is now live.' }
-      else
-        format.html { render :new }
+      @portfolio_item = Portfolio.new(portfolio_params)
+      
+      respond_to do |format|
+        if @portfolio_item.save
+          format.html { redirect_to portfolios_path, notice: 'Your portfolio item  is now live.' }
+        else
+          format.html { render :new }
+        end
       end
-    end
     end
   
   def edit
-  @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
   end
   
   def update
       @portfolio_item = Portfolio.find(params[:id])
       
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
     end
   end
+  
 def show
     @portfolio_item = Portfolio.find(params[:id])
 end
@@ -56,4 +57,14 @@ def destroy
     end
 end
 
+private
+ 
+  def portfolio_params
+     params.require(:portfolio).permit(:title,
+                                       :subtitle,
+                                       :body,
+                                       technologies_attributes: [:name]
+                                      )
+  end
+ 
 end
