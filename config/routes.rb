8fc26a7d812de5881d8802fd7da590e86f54e954
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-08-01 15:18:15 +0000

    addes session tracked for dynamo=ic source links

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
